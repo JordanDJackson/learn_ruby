d84680f0909409ec68d8ab2e6f50e458f2a6778d
Jordan D. Jackson <jordandjackson21@gmail.com>
2016-08-15 19:11:29 -0700

    capital

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,38 @@
-#write your code here
+def echo x
+	return x
+end
+def shout x
+	return x.upcase
+end
+def repeat (x,*y)
+	rep = ""
+	if y == []
+		return x + " " + x
+	else
+		(y[0]).times do
+			rep += x+" "
+		end
+		return rep.strip
+	end
+end
+def start_of_word x,y
+	return x[0...y]
+end
+
+def first_word x
+	arr = x.split(" ")
+	return arr[0]
+end
+
+def titleize x
+	puncs = ["and", "the", "to", "of", "by", "from", "or"]
+	arr = x.split(" ")
+	if arr.length <= 1
+		x.capitalize
+	else
+		x.split.map(&:capitalize).join(' ')
+	end
+
+
+
+end
