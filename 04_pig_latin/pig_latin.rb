e5e80018db8eb75d56ab978e63456fa359c11b0d
Jordan D. Jackson <jordandjackson21@gmail.com>
2016-08-18 01:58:49 -0700

    piglatin

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,6 +1,22 @@
 def translate s
-	vowels = ['a','e','i','o','u']
-	s.split(" ")
-	return s
+	sep = s.split(" ")
+	arr = []
+	sep.each do |word|
+		if /[aeiou]/.match(word[0])
+			word = word + 'ay'
+			arr << word
+		elsif /[^aeiou][^aeiou][^aeiou]/.match(word[0...3])|| /squ/.match(word[0...3])
+			word = word[3...word.length] + word[0..2] + 'ay'
+			arr << word	
+		elsif /[^aeiou][^aeiou]/.match(word[0...2]) || /qu/.match(word[0...2])
+			word = word[2...word.length] + word[0..1] + 'ay'
+			arr << word	
+		elsif /[^aeiou]/.match(word[0])
+			word = word[1...word.length] + word[0] + 'ay'
+			arr << word	
+		end	
+	end
+return arr.join(" ")
+
 
 end
